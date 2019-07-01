a5cc33cf747734aea7a547030ec3912eb8fe86a5
Administrator <xiang4869@qq.com>
2019-07-01 17:51:53 +0800

    init

--- fateboard-web/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"http://172.16.153.113:16688"',
-  WEBSOCKET_BASE_API: '"ws://172.16.153.113:16688"'
-})
--- fateboard-web/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://172.16.153.113:16688"',
-  WEBSOCKET_BASE_API: '"ws://172.16.153.113:16688"'
-}
