631b65557331a1e64f5decb956634a826421557b
mgqa34 <mgq3374541@163.com>
2019-07-15 21:37:22 +0800

    update transfer variable generator with reading template in header and generate other template

--- federatedml/util/transfer_variable/transfer_variable_template.py
+++ federatedml/util/transfer_variable/transfer_variable_template.py
@@ -27,7 +27,3 @@ from federatedml.util.transfer_variable.base_transfer_variable import BaseTransf
 from federatedml.util.transfer_variable.base_transfer_variable import Variable
 
 
-class SampleTransferVariable(BaseTransferVariable):
-    def define_transfer_variable(self):
-        self.sample_ids = Variable(name="SampleTransferVariable.sample_ids", auth={'src': "guest", 'dst': ['host']})
-        pass
